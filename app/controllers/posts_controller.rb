edbd137e6270de90fee5b09f1d2a0764f493ed17
Jaroslaw Zmijewski <jzmija@wp.pl>
2019-07-27 02:10:07 +0200

    Create all views and controller for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,55 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index]
+  
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+  	@post = Post.new(post_params)
+      if @post.save
+        flash[:notice] = "Utworzyłeś post"
+        redirect_to post_path(@post)
+      else
+        render 'new'
+      end
+  end
+
+  def show
   end
 
   def edit
   end
 
-  def destroy
+  def update
+	  if  @post.update(post_params)
+      flash[:notice] = "Zaktualizowałeś post"
+	    redirect_to post_path(@post)
+	  else
+		  render 'edit'
+	  end
   end
+
+  def destroy
+    @post.destroy
+    flash[:alert] = "Usunąłeś post"
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title,:description)
+
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  
+	validates :title, presence: true, length: { minimum: 5}
+	validates :desctiption, presence: true, length: { minimum: 20}
+
+	belongs_to :user
 end
